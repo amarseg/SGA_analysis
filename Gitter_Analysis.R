6e965f7798b2e7561213e527b4ea25f9fbfa69b4
Unknown <liamarseg@gmail.com>
2018-07-30 15:48:34 +0100

    gitter script now works!

--- Gitter_Analysis.R
+++ Gitter_Analysis.R
@@ -1,8 +1,13 @@
 library(gitter)
-setwd("C:/Users/am4613/Desktop/150618_SGA/")
 
-files <- list.files(recursive = T)
+files <- list.files(path = "data/Amalia/150618_SGA/", recursive = T, full.names = T)
 files <- files[grepl("jpg", files)]
 
 dir.create("Analysed_Images")
 gitter.batch(files, plate.format = c(16, 24), inverse = T, dat.save="Analysed_Images", grid.save="Analysed_Images")
+
+files <- list.files(path = "data/Amalia/180718_SGA/", recursive = T, full.names = T)
+files <- files[grepl("jpg", files)]
+
+dir.create("data/Amalia/180718_SGA/Analysed_Images")
+gitter.batch(files, plate.format = c(16, 24), inverse = T, dat.save="data/Amalia/180718_SGA/Analysed_Images", grid.save="data/Amalia/180718_SGA/Analysed_Images")
